5c9bd860cf200cc5dd9107320a3d6e48c38a4e0d
Sheetal Mohite <smohite@tavisca.com>
2015-05-27 18:54:53 +0530

    project added

---
+++ Supplier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Supplier
+{
+	public Supplier()
+	{
+	}
+
+    public int SupplierId { get; set; }
+    public string ProductType { get; set; }
+    public string  { get; set; }
+}
